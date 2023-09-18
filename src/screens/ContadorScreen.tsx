132c9ca03ae0c0df00d8b4023f66702ad9dec504
MartinB04 <martin.bibiano4@outlook.com>
2023-09-18 14:54:34 -0600

    ContadorScreen: floating action button +1 recolocado, agregado fab -1

--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -8,20 +8,55 @@ export const ContadorScreen = () => {
     <View style={style.container}>
       <Text style={style.title}>Contador: {contador}</Text>
 
-      <TouchableOpacity style={{backgroundColor: 'red', width: 200}}>
-        <Text style={style.button}>
-          Presioname Bv !!!
-        </Text>
+      <TouchableOpacity
+        onPress={() => setContador(contador - 1)}
+        style={[style.fabLocation, style.fabLocationL]}>
+        <View style={style.fabButton}>
+          <Text style={style.fabText}>-1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => setContador(contador + 1)}
+        style={[style.fabLocation, style.fabLocationR]}>
+        <View style={style.fabButton}>
+          <Text style={style.fabText}>+1</Text>
+        </View>
       </TouchableOpacity>
     </View>
   );
 };
 
 const style = StyleSheet.create({
-    container: {flex: 1, justifyContent: 'center'},
-title: {textAlign: 'center', fontSize: 25, top: -15},
-button: {
-    fontSize: 20, textAlign: 'center'
-}
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 25,
+    top: -15,
+  },
+  fabLocation: {
+    position: 'absolute',
+    bottom: 10,
+  },
+  fabLocationR: {
+    right: 15,
+  },
+  fabLocationL: {
+    left: 15,
+  },
+  fabButton: {
+    backgroundColor: 'blue',
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    justifyContent: 'center',
+  },
 
+  fabText: {
+    fontSize: 20,
+    color: 'white',
+    alignSelf: 'center',
+  },
 });
