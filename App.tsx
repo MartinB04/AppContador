53e170a0fcc9df09d87b8d81572784daea279a96
MartinB04 <martin.bibiano4@outlook.com>
2023-09-17 23:14:16 -0600

    Primer commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
+
+export const App = () => {
+  return (
+    <HolaMundoScreen/>
+  );
+};
